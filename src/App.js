0f6bbc5fd14f61df32cedbc93833346f4014620d
Antonio Seveso <a.seveso@ianum.com>
2021-05-28 12:47:45 +0200

    - greets everyone

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello to AWS Basic Course members!
         </p>
         <a
           className="App-link"
